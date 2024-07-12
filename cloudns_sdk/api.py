fcbc03ee8adeaddb819bee0440a31d9cd1cb107c
lively-ops <74601111+lively-ops@users.noreply.github.com>
2024-07-12 23:59:33 +0545

    added cloud domain methods

--- cloudns_sdk/api.py
+++ cloudns_sdk/api.py
@@ -401,6 +401,24 @@ class ClouDNSAPI:
 
 
 
+    def add_cloud_domain(self, domain_name, cloud_domain_name):
+        params = self._auth_params({'domain-name': domain_name, 'cloud-domain-name': cloud_domain_name})
+        return self.make_request('dns/add-cloud-domain.json', method='POST', params=params)
+
+    def delete_cloud_domain(self, domain_name):
+        params = self._auth_params({'domain-name': domain_name})
+        return self.make_request('dns/delete-cloud-domain.json', method='POST', params=params)
+
+    def change_cloud_master(self, domain_name):
+        params = self._auth_params({'domain-name': domain_name})
+        return self.make_request('dns/set-master-cloud-domain.json', method='POST', params=params)
+
+    def list_cloud_domains(self, domain_name):
+        params = self._auth_params({'domain-name': domain_name})
+        return self.make_request('dns/list-cloud-domains.json', method='POST', params=params)
+
+
+
 
 
 
