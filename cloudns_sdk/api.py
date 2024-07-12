603f06334da0667b085abcd3db08cbbd1506c451
lively-ops <74601111+lively-ops@users.noreply.github.com>
2024-07-12 23:53:14 +0545

    added mail forward support

--- cloudns_sdk/api.py
+++ cloudns_sdk/api.py
@@ -370,6 +370,43 @@ class ClouDNSAPI:
 
 
 
+    def get_mail_forward_stats(self):
+        params = self._auth_params()
+        return self.make_request('dns/get-mail-forwards-stats.json', method='GET', params=params)
+
+
+    def get_mail_forward_servers(self):
+        params = self._auth_params()
+        return self.make_request('dns/get-mailforward-servers.json', method='GET', params=params)
+
+    def add_mail_forward(self, domain_name, box='', host='', destination=''):
+        params = self._auth_params({'domain-name': domain_name, 'box': box, 'host': host, 'destination': destination})
+        return self.make_request('dns/add-mail-forward.json', method='POST', params=params)
+
+    def modify_mail_forward(self, domain_name, box='', host='', destination='', mail_forward_id=None):
+        params = self._auth_params({'domain-name': domain_name, 'box': box, 'host': host, 'destination': destination, 'mail-forward-id': mail_forward_id})
+        return self.make_request('dns/modify-mail-forward.json', method='POST', params=params)
+
+    def delete_mail_forward(self, domain_name, mail_forward_id):
+        params = self._auth_params({'domain-name': domain_name, 'mail-forward-id': mail_forward_id})
+        return self.make_request('dns/delete-mail-forward.json', method='POST', params=params)
+
+    def list_mail_forwards(self, domain_name):
+        params = self._auth_params({'domain-name': domain_name})
+        return self.make_request('dns/mail-forwards.json', method='GET', params=params)
+
+    def change_mail_forward_status(self, domain_name, mail_forward_id, status=True):
+        params = self._auth_params({'domain-name': domain_name, 'mail-forward-id': mail_forward_id, 'status': 1 if status else 0})
+        return self.make_request('dns/modify-mail-forward-status.json', method='POST', params=params)
+
+
+
+
+
+
+
+
+
 
 
 
