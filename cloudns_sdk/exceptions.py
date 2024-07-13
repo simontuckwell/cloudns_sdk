8feca69b07a2785d48f52b4d1bd404bb70a50052
lively-ops <74601111+lively-ops@users.noreply.github.com>
2024-07-13 17:39:08 +0545

    Update exceptions.py

--- cloudns_sdk/exceptions.py
+++ cloudns_sdk/exceptions.py
@@ -1,7 +1,7 @@
 class ClouDNSAPIException(Exception):
     def __init__(self, response):
         self.status = response.get('status')
-        self.description = response.get('status-description')
+        self.description = response.get('statusDescription')
         super().__init__(self.description)
 
     def __str__(self):
