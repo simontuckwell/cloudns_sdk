8b11de9442c3d372672696d64a9f4adbcbade924
lively-ops <74601111+lively-ops@users.noreply.github.com>
2024-07-13 20:19:00 +0545

    Update example.py

--- example.py
+++ example.py
@@ -2,13 +2,13 @@
 from cloudns_sdk import ClouDNSAPI
 from cloudns_sdk.validations import validate
 
-id = 22969
-key = '152207'
+id = 101010
+key = '1@mypass'
 
 api = ClouDNSAPI(id, key)
 
 
-res = api.zone.records.add_record('jointcove.com', 'A', '1.3.3.1')
+res = api.zone.records.add_record('example.com', 'A', '1.3.3.1')
 
 print(res)
 
